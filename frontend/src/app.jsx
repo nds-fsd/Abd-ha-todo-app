e2e5e4c9b5fc7bcc41242db24c69cb6985e9c99a
Abd-ha <haouchineabdelhak@gmail.com>
2024-03-03 21:08:41 +0100

    Changes to be committed: 	modified:   frontend/src/app.jsx 	modified:   frontend/src/components/logreg/login.jsx 	modified:   frontend/src/main.jsx

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,24 +1,7 @@
 import { useEffect, useState } from "react";
-import {BrowserRouter, Routes, Route } from 'react-router-dom'
-import LandingPage from "./landingpage.jsx";
-import Signup from "./components/logreg/signup.jsx"
-import Login from "./components/logreg/login.jsx"
 import Task from "./components/taskform/task.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "./index.css"
-      //export function Pages() {
-//  return(
-//    <BrowserRouter>
-//      <Navigation />
-//      <Routes>
-//        <Route path="/landingpage" element={<LandingPage />}></Route>
-//        <Route path="/signup" element={<Signup />}></Route>
-//        <Route path="/login" element={<Login />}></Route>
-//        <Route path="/task" element={<Task />}></Route>
-//      </Routes>
-//    </BrowserRouter>
-//  )
-// }
 
 
 export default function App() {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LandingPage from './landingpage.jsx'
 import App from './app'
-import './index.css'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/app" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
