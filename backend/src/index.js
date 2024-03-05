105a36151f3251ca0086adf3ff76f6c53a58194e
Abd-ha <haouchineabdelhak@gmail.com>
2024-03-05 11:47:32 +0100

    Changes to be committed: 	modified:   backend/src/index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,13 +3,14 @@ const express = require('express');
 const {connectDB} =  require("./mongo/connection");
 const cors = require('cors');
 const {taskRouter} =  require("./controllers/task");
-////const {UserRouter} =  require("./controllers/auth");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(taskRouter)
-////app.use(userRouter)
+app.use(taskRouter);
+
+
 
 connectDB().then(() => console.log("Connected to database!"))
 
